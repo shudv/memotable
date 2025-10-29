9ba1119096fa349a6de225369335ecfbcdb210bb
Shubham <6473147+shudv@users.noreply.github.com>
2025-10-29 13:25:03 +0530

    Reduce bundle size and increase compatibility(#21)
    Co-authored-by: Shubham Dwivedi <shudv@users.noreply.github.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/index.ts"],
-    format: ["esm", "cjs"],
+    entry: ["src/index.ts", "src/integrations/React.ts"],
+    format: ["cjs", "esm"],
     dts: true,
-    sourcemap: true,
-    clean: true,
     minify: true,
-    target: "es2019",
+    clean: true,
+    splitting: false,
+    sourcemap: true,
+    esbuildOptions(options) {
+        options.mangleProps = /^_/; // Mangle properties starting with underscore
+    },
+    terserOptions: {
+        mangle: {
+            properties: {
+                regex: /^_/, // Mangle private properties (starting with _)
+            },
+        },
+        compress: {
+            passes: 5,
+        },
+    },
 });
